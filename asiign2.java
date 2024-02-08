199796a2d69b21210784ec5e780ab7fe33d7ddc0
rajas <vrr9393@gmail.com>
2024-02-07 21:58:51 -0600

    Changing E to C

--- asiign2.java
+++ asiign2.java
@@ -4,12 +4,12 @@ public class asiign2 {
         int resultA = -5 + 8 * 6;
         int resultB = (55 + 9) % 9;
         double resultE = 20 + -3 * 5 / 8;
-        double resultD = 5 + 15 / 3 * 2 - 8 % 3;
+        double resultF = 5 + 15 / 3 * 2 - 8 % 3;
 
         // Print Results
         System.out.println("Result of -5 + 8 * 6 = " + resultA);
         System.out.println("Result of (55 + 9) % 9 = " + resultB);
-        System.out.println("Result of 20 + -3 * 5 / 8 = " + resultC);
-        System.out.println("Result of 5 + 15 / 3 * 2 - 8 % 3 = " + resultD);
+        System.out.println("Result of 20 + -3 * 5 / 8 = " + resultE);
+        System.out.println("Result of 5 + 15 / 3 * 2 - 8 % 3 = " + resultF);
     }
 }
